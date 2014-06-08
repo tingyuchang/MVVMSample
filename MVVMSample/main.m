89175c7faf1d6d915b92ab0cf290146b3c9ff671
Matt Chang <mattchang@accuvally.com>
2014-06-08 10:50:55 +0800

    Initial Commit

---
+++ MVVMSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MVVMSample
+//
+//  Created by Matt Chang on 2014/6/8.
+//  Copyright (c) 2014年 Accuvally Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}
